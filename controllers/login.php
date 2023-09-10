bcb2865d6c1803009d1a433263a098715cde08c8
Cybrosif <anej.dolercrep@gmail.com>
2023-09-10 17:36:14 +0200

    test

---
+++ controllers/login.php
@@ -0,0 +1,22 @@
+<?php
+          include '../links/db.php';
+
+          if (isset($_POST['login'])) {
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+        }
+        else{
+            header("Location: ../index.php");
+    
+        }
+
+        $hash = password_hash($password, PASSWORD_BCRYPT);
+
+         $stmt = $link->prepare("SELECT * FROM Uporabniki WHERE Email = ? AND Geslo = ?");
+          $stmt->bind_param("ss", $email, $hash);
+          $stmt->execute();
+          $stmt->bind_result($id, $ime, $priimek, $vloga, $geslo,$email);
+          $stmt->close();
+
+          echo 'id';
+        ?>
--- views/login_page.php
+++ views/login_page.php
@@ -41,35 +41,11 @@
           margin-left:140px;
         }
 </style>
-        <?php
-          /*include '../links/db.php';
-
-          $imeToSearch = "Anej";
-
-          $stmt = $link->prepare("SELECT * FROM Uporabniki WHERE Ime = ?");
-          $stmt->bind_param("s", $imeToSearch);
-          $stmt->execute();
-          $result = $stmt->get_result();
-
-          while ($row = $result->fetch_assoc()) {
-              $id = $row['ID'];
-              $Ime = $row['Ime'];
-              $Priimek = $row['Priimek'];
-              $Vloga = $row['Vloga'];
-              $Geslo = $row['Geslo'];
-              $Email = $row['Email'];
-
-              echo "ID: $id, Ime: $Ime, Priimek: $Priimek, Vloga: $Vloga, Geslo: $Geslo, Email: $Email<br>";
-          }
-
-          $stmt->close();*/
-
-        ?>
   <body> 
         <div class="container d-flex flex-row border-0 shadow rounded">
             <div class="card border-0">
                 <div class="card-body">
-                    <form action="">
+                    <form action="./controllers/login.php">
                     <svg class="form-control border-0 my-3" xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                     <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
@@ -78,7 +54,7 @@
                     <input type="text" name="" id="" class="form-control my-2 py-2" placeholder="Geslo" />
                     <div class="my-4"></div>
                     <div class="text-center mt-1">
-                        <button class="btn btn-primary ">Prijava</button>
+                        <button class="btn btn-primary " name='login' >Prijava</button>
                         <a href="registration_page.php" class="nav-link">Še nimate profila?</a>
                     </div>
                     </form>
