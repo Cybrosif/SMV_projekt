443f1d4cdf7dd39c41702300dcf0cbded8401e13
Cybrosif <anej.dolercrep@gmail.com>
2023-09-10 17:42:03 +0200

    fix

--- controllers/login.php
+++ controllers/login.php
@@ -15,7 +15,7 @@
          $stmt = $link->prepare("SELECT * FROM Uporabniki WHERE Email = ? AND Geslo = ?");
           $stmt->bind_param("ss", $email, $hash);
           $stmt->execute();
-          $stmt->bind_result($id, $ime, $priimek, $vloga, $geslo,$email);
+          $stmt->bind_result($id, $ime, $priimek, $vloga, $geslo, $email);
           $stmt->close();
 
           echo 'id';
