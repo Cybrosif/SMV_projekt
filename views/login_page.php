75ccd69e01b44eeae5c09334f1d474bccd73dde9
Cybrosif <anej.dolercrep@gmail.com>
2023-09-10 16:58:29 +0200

    login popravek + test

--- views/login_page.php
+++ views/login_page.php
@@ -41,21 +41,31 @@
           margin-left:140px;
         }
 </style>
+        <?php
+          include '../database.php';
+          $stmt = $link->prepare("SELECT * FROM Uporabniki WHERE Ime = ?");
+          $stmt->bind_param("s", "Anej");
+          $stmt->execute();
+          $stmt->bind_result($id, $Ime, $Priimek, $Vloga, $Geslo,$Email);
+          $stmt->fetch();
 
+          echo $id;
+        ?>
   <body> 
         <div class="container d-flex flex-row border-0 shadow rounded">
             <div class="card border-0">
                 <div class="card-body">
                     <form action="">
-                    <svg class="form-control border-0 my-3" xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <svg class="form-control border-0 my-3" xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                     <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
                     </svg>
-                    <input type="text" name="" id="" class="form-control my-4 py-2" placeholder="Email" />
-                    <input type="text" name="" id="" class="form-control my-4 py-2" placeholder="Geslo" />
+                    <input type="text" name="" id="" class="form-control my-2 py-2" placeholder="Email" />
+                    <input type="text" name="" id="" class="form-control my-2 py-2" placeholder="Geslo" />
+                    <div class="my-4"></div>
                     <div class="text-center mt-1">
-                        <button class="btn btn-primary">Prijava</button>
-                        <a href="registration_page.php" class="nav-link my-2">Še nimate profila?</a>
+                        <button class="btn btn-primary ">Prijava</button>
+                        <a href="registration_page.php" class="nav-link">Še nimate profila?</a>
                     </div>
                     </form>
                 </div>
