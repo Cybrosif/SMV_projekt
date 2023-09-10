98cd0ce1e2b2ea7a0359121504ce5c7b9b0ff92e
Cybrosif <anej.dolercrep@gmail.com>
2023-09-10 17:47:25 +0200

    pp

--- controllers/login.php
+++ controllers/login.php
@@ -12,7 +12,7 @@
         $hash = password_hash($password, PASSWORD_BCRYPT);
 
          $stmt = $link->prepare("SELECT * FROM Uporabniki WHERE Email = ? AND Geslo = ?");
-          $stmt->bind_param("ss", $email, $hash);
+          $stmt->bind_param("ss", $email, $password);
           $stmt->execute();
           $stmt->bind_result($id, $ime, $priimek, $vloga, $geslo, $email);
           $stmt->close();
