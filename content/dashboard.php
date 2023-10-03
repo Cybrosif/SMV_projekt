ca157efa3ffd1936b72ec4a516cf24ee0f7e1ba2
Kopriuc <gasper.koprivc1@gmail.com>
2023-10-03 11:47:16 +0200

    grid

--- content/dashboard.php
+++ content/dashboard.php
@@ -1,4 +1,32 @@
 <?php
     
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 <h1 class='text-center primary-text'>Nadzorna plošča</h1>
+<body>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      Profesorji
+    </div>
+    <div class="col">
+      Dodeljene naloge
+    </div>
+  </div>
+</div>
+</body>
+</html>
+
+<style>
+</style>
+
+
+
+
+
