c11cd83a65de88f83254cb4845ff1e87750664d0
Cybrosif <anej.dolercrep@gmail.com>
2023-10-04 19:02:53 +0200

    fix

--- content/dashboard.php
+++ content/dashboard.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 <h1 class='text-center primary-text'>Nadzorna plošča</h1>
 <body>
 <div class="container">
