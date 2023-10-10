2e510755e5138d26f5dc9dd4dbb1de6e2482e0ae
ArPlays <sandisandi709@gmail.com>
2023-10-10 11:54:36 +0200

    New looks

--- content/specific_class.php
+++ content/specific_class.php
@@ -23,28 +23,39 @@ $nalogeResult = mysqli_query($link, $nalogeQuery);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Specific Class</title>
 
-
+    <!-- Bootstrap 4 CSS and necessary scripts -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
+    <!-- Optional: Custom CSS to match home.php styling -->
+    <style>
+        /* Example styles to match assumed home.php appearance */
+        body {
+            background-color: #f5f5f5;
+        }
+        .container {
+            background-color: #ffffff;
+            padding: 2rem;
+            border-radius: 5px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+        }
+    </style>
 </head>
 <body>
 
-    <div class="container mt-5"> 
+<div class="container mt-5">
+    <h1 class="text-center mb-5"><?php echo $className; ?></h1>
 
-        <h2 class="text-center mb-4"> 
-            <?php echo $className; ?>
-        </h2>
-
-
-        <h3>Naloge:</h3>
-        <ul class="list-group"> 
+    <div class="card">
+        <div class="card-header bg-primary text-white">
+            <h3>Naloge:</h3>
+        </div>
+        <ul class="list-group list-group-flush">
         <?php
             while($nalogeData = mysqli_fetch_assoc($nalogeResult)) {
-                echo '<li class="list-group-item">'; // Added Bootstrap 'list-group-item' class
+                echo '<li class="list-group-item">';
                 echo "<strong>Naslov:</strong> " . $nalogeData['Naslov'] . "<br>";
                 echo "<strong>Opis:</strong> " . $nalogeData['Opis'] . "<br>";
                 echo "<strong>Rok:</strong> " . $nalogeData['Rok'];
@@ -52,8 +63,8 @@ $nalogeResult = mysqli_query($link, $nalogeQuery);
             }
         ?>
         </ul>
-
     </div>
+</div>
 
 </body>
 </html>
