94c2a630d79ab4cc4a0daede89d8d13a624312f7
ArPlays <sandisandi709@gmail.com>
2023-10-30 12:19:30 +0100

    Popravek na dashboardu

--- content/dashboard.php
+++ content/dashboard.php
@@ -4,15 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        .custom-date-color {
+            color: black !important;
+        }
+    </style>
 </head>
 
 <body class="bg-light">
 
-    <div class="container mt-5">
-        <h1 class="text-center text-primary mb-5">Nadzorna plošča</h1>
+    <div class="container mt-3">
+        <h1 class="text-center text-primary mb-4">Nadzorna plošča</h1>
 
         <div class="row mt-3">
-            <!-- Moji predmeti Section -->
             <div class="col-lg-6 mb-4">
                 <div class="card shadow">
                     <div class="card-header bg-primary text-white">
@@ -21,7 +25,6 @@
                     <div class="card-body">
                         <?php
                         include("../../db.php");
-
                         if (isset($_SESSION['user_id'])) {
                             $uporabnik_id = $_SESSION['user_id'];
                             $sql = "SELECT r.Ime_razreda
@@ -29,7 +32,6 @@
                                     INNER JOIN razredi AS r ON ur.Razred_ID = r.Razred_ID
                                     WHERE ur.Uporabnik_ID = $uporabnik_id";
                             $result = $link->query($sql);
-
                             if ($result->num_rows > 0) {
                                 echo '<ul class="list-group">';
                                 while ($row = $result->fetch_assoc()) {
@@ -43,24 +45,21 @@
                         } else {
                             echo '<div class="alert alert-danger">Uporabnik ni prijavljen.</div>';
                         }
-
                         $link->close();
                         ?>
                     </div>
                 </div>
             </div>
 
-            <!-- Dodeljene naloge Section -->
             <div class="col-lg-6">
                 <div class="card shadow">
                     <div class="card-header bg-primary text-white">
                         <h4>Dodeljene naloge</h4>
                     </div>
-                    <div class="card-body" style="max-height: 300px; overflow-y: auto;">
+                    <div class="card-body" style="max-height: 350px; overflow-y: auto;">
                         <div class="list-group">
                             <?php
                             include("../../db.php");
-
                             if (isset($_SESSION['user_id'])) {
                                 $uporabnik_id = $_SESSION['user_id'];
                                 $sql = "SELECT n.Naslov, n.Rok FROM naloge AS n
@@ -68,19 +67,16 @@
                                         WHERE ur.Uporabnik_ID = $uporabnik_id
                                         AND n.Rok <= DATE_ADD(CURDATE(), INTERVAL 1 WEEK)
                                         ORDER BY n.Rok ASC";
-
                                 $result = $link->query($sql);
-
                                 if ($result->num_rows > 0) {
                                     while ($row = $result->fetch_assoc()) {
                                         $rok = $row["Rok"];
                                         $naslov = $row["Naslov"];
                                         $datum_roka = strtotime($rok);
-
                                         $class = ($datum_roka < strtotime("today")) ? 'list-group-item-danger' : '';
                                         echo '<a href="#" class="list-group-item list-group-item-action ' . $class . ' d-flex justify-content-between align-items-center">';
                                         echo $naslov;
-                                        echo '<span class="badge badge-primary badge-pill">' . date('d.m.Y', $datum_roka) . '</span>';
+                                        echo '<span class="badge badge-primary badge-pill custom-date-color">' . date('d.m.Y', $datum_roka) . '</span>';
                                         echo '</a>';
                                     }
                                 } else {
@@ -89,7 +85,6 @@
                             } else {
                                 echo '<div class="alert alert-danger">Uporabnik ni prijavljen.</div>';
                             }
-
                             $link->close();
                             ?>
                         </div>
