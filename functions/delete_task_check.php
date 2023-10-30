036b470bdc44b5442d6533bb88fde11a08a2b9e2
ArPlays <sandisandi709@gmail.com>
2023-10-30 13:24:18 +0100

    Delete task preverjanje

---
+++ functions/delete_task_check.php
@@ -0,0 +1,16 @@
+<?php
+if ($_SESSION['user_vloga'] != 'Profesor') {
+    die("Unauthorized access!");
+}
+
+// If user is a professor, check if they are associated with the class
+if ($_SESSION['role'] == 'Profesor') {
+    $classId = $_POST['classId'];
+    $sql = "SELECT * FROM ucitelji_razredi WHERE Ucitelj_ID = $_SESSION['user_id'] AND Razred_ID = $classId";
+    $result = $link->query($sql);
+    if ($result->num_rows == 0) {
+        die("You do not have permission to delete this class.");
+    }
+}
+
+?>
