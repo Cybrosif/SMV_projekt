1d6b36af8b8020fc74416ba30d1ec7493bba2b37
ArPlays <sandisandi709@gmail.com>
2023-10-30 16:45:11 +0100

    Fixes

--- functions/delete_task_check.php
+++ functions/delete_task_check.php
@@ -6,7 +6,9 @@ if ($_SESSION['user_vloga'] != 'Profesor') {
 // If user is a professor, check if they are associated with the class
 if ($_SESSION['user_vloga'] == 'Profesor') {
     $classId = $_POST['razredId'];
-    $sql = "SELECT * FROM ucitelji_razredi WHERE Ucitelj_ID = $_SESSION['user_id'] AND Razred_ID = $classId";
+    $userId = $_SESSION['user_id'];
+
+    $sql = "SELECT * FROM ucitelji_razredi WHERE Ucitelj_ID = $userId AND Razred_ID = $classId";
     $result = $link->query($sql);
     if ($result->num_rows == 0) {
         die("You do not have permission to delete this class.");
