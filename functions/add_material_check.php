39e54a2418e70b94e252980882ada83c87926c29
ArPlays <sandisandi709@gmail.com>
2023-10-30 13:11:12 +0100

    Dodano preverjanje za add material modal

---
+++ functions/add_material_check.php
@@ -0,0 +1,15 @@
+<?php 
+if ($_SESSION['user_vloga'] != 'Profesor' && $_SESSION['user_vloga'] != 'Admin') {
+    die("Unauthorized access!");
+}
+
+$classId = $_POST['classId']; 
+$sql = "SELECT * FROM ucitelji_razredi WHERE Ucitelj_ID = $_SESSION['userId'] AND Razred_ID = $classId";
+$result = $link->query($sql);
+if ($result->num_rows == 0) {
+    die("You do not have permission to upload material for this class.");
+}
+
+
+
+?>
