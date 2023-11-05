fa3736a03520ec06455a6a12af9530d2e3aa5d6c
Cybrosif <anej.dolercrep@gmail.com>
2023-11-05 15:09:18 +0100

    test

--- functions/add_material_check.php
+++ functions/add_material_check.php
@@ -1,17 +1,14 @@
 <?php 
-if ($_SESSION['user_vloga'] != 'Profesor' && $_SESSION['user_vloga'] != 'Administrator') {
-    die("Unauthorized access!");
-}
+    if ($_SESSION['user_vloga'] != 'Profesor' && $_SESSION['user_vloga'] != 'Administrator') {
+        die("Unauthorized access!");
+    }
 
-$classId = $_POST['classId']; 
-$userId = $_SESSION['user_id'];
+    $classId = $_POST['classId']; 
+    $userId = $_SESSION['user_id'];
 
-$sql = "SELECT * FROM ucitelji_razredi WHERE Ucitelj_ID = $userId AND Razred_ID = $classId";
-$result = $link->query($sql);
-if ($result->num_rows == 0) {
-    die("You do not have permission to upload material for this class.");
-}
-
-
-
+    $sql = "SELECT * FROM ucitelji_razredi WHERE Ucitelj_ID = $userId AND Razred_ID = $classId";
+    $result = $link->query($sql);
+    if ($result->num_rows == 0) {
+        die("You do not have permission to upload material for this class.");
+    }
 ?>
