1fbd726f2dfdce565269f21e8c4e3786afb3c3d8
ArPlays <sandisandi709@gmail.com>
2023-11-05 19:52:57 +0100

    Testiranje

--- controllers/delete_user.php
+++ controllers/delete_user.php
@@ -3,16 +3,45 @@ include '../session_start.php';
 include '../../db.php';
 include '../functions/check_if_admin.php';
 
+// Function to delete files from the server
+function deleteFileFromServer($filePath) {
+    $baseDir = '../uploads/'; 
+    $fullPath = $baseDir . $filePath;
+
+    if (file_exists($fullPath)) {
+        if (!unlink($fullPath)) {
+            error_log("Failed to delete file: " . $fullPath);
+        }
+    } else {
+        error_log("File not found: " . $fullPath);
+    }
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $userId = $_POST['userId'];
 
-    // Perform deletion operation in the database (Example query, update with your query)
-    $sql = "DELETE FROM uporabniki WHERE ID = $userId";
+    // Select all file paths associated with the user
+    $query = "SELECT Pot_Do_Datoteke FROM student_naloge WHERE Student_ID = ?";
+    if ($stmt = mysqli_prepare($link, $query)) {
+        mysqli_stmt_bind_param($stmt, "i", $userId);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        while ($row = mysqli_fetch_assoc($result)) {
+            deleteFileFromServer($row['Pot_Do_Datoteke']);
+        }
+        mysqli_stmt_close($stmt);
+    }
 
-    if ($link->query($sql) === true) {
-        echo "Record inserted successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $link->error;
+    // Perform deletion operation in the database
+    $sql = "DELETE FROM uporabniki WHERE ID = ?";
+    if ($stmt = mysqli_prepare($link, $sql)) {
+        mysqli_stmt_bind_param($stmt, "i", $userId);
+        if (mysqli_stmt_execute($stmt)) {
+            echo "User and associated files deleted successfully";
+        } else {
+            echo "Error: " . mysqli_stmt_error($stmt);
+        }
+        mysqli_stmt_close($stmt);
     }
 } else {
     echo 'Invalid request';
